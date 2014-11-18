3963557eff53aa06d92e847f56425821fa887b41
Jiahao Li <isundaylee.reg@gmail.com>
2014-11-18 16:04:17 -0500

    Adds active status.

--- app/models/brother.rb
+++ app/models/brother.rb
@@ -4,4 +4,11 @@ class Brother < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 1, maximum: 100}
   validates :year, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1900, less_than_or_equal_to: 3000}
   validates :pledge_class, presence: true
+  validates :current, :inclusion => {:in => [true, false]}
+
+  after_initialize do
+    if new_record?
+      self.current = true
+    end
+  end
 end
