a6220076299005c73aac2edc9353a8b213dd20c7
Jiahao Li <isundaylee.reg@gmail.com>
2014-11-18 16:16:21 -0500

    Basic layouts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  root 'static_pages#homepage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
