2be5f8fd3db9b8b0c74de5cccd14de3d044b2d78
Jiahao Li <isundaylee.reg@gmail.com>
2014-11-18 15:47:18 -0500

    Pledge class model.

---
+++ app/models/pledge_class.rb
@@ -0,0 +1,4 @@
+class PledgeClass < ActiveRecord::Base
+  validates :name, presence: true, length: {minimum: 1, maximum: 50}
+  validates :year, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1900, less_than_or_equal_to: 3000}
+end
