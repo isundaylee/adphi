7be0b87c4188c0bf723a592ee9f2721f7635e1df
Jiahao Li <isundaylee.reg@gmail.com>
2014-11-19 10:42:40 -0500

    Redirect back instead of root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :store_location
 
   private
+
+    # Redirect logic
+
+    def store_location
+      session[:return_to] = request.fullpath if request.get? and controller_name != "sessions"
+    end
+
+    def redirect_back(default = root_url, params)
+      redirect_to(session[:return_to] || default, params)
+    end
 
     # Authentication
 
     def require_signed_in
       unless signed_in?
-        redirect_to root_url, flash: {alert: 'You need to be signed in. '}
+        redirect_back root_url, flash: {alert: 'You need to be signed in. '}
       end
     end
 
