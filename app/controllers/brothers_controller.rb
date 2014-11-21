3db727eeb720434fa31a5ecf1b211017fc9b8480
Jiahao Li <isundaylee.reg@gmail.com>
2014-11-21 15:37:30 -0500

    Reduces # of SQL queries.

--- app/controllers/brothers_controller.rb
+++ app/controllers/brothers_controller.rb
@@ -2,11 +2,20 @@ class BrothersController < ApplicationController
   load_and_authorize_resource
 
   def show
-    # @brother = Brother.find(params[:id])
     @logs = @brother.shortlogs.order('created_at DESC').map { |x| x.to_summary_entry }.compact
   end
 
   def index
     @brothers = Brother.current
+    @balances = Balance.all.group_by { |x| x.brother_id }.hmap do |b, bs|
+      [
+        b,
+        {
+          kitchen: bs.select { |x| x.kitchen? } [0] || Brother.find(b).balance(:kitchen),
+          house: bs.select { |x| x.house? } [0] || Brother.find(b).balance(:house),
+          social: bs.select { |x| x.social? } [0] || Brother.find(b).balance(:social)
+        }
+      ]
+    end
   end
 end
