5230f9ec37bea3c2c98057abac28c68471b990dd
bojanserafimov <boki@mit.edu>
2015-07-03 20:50:15 -0400

    Write groups views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,14 +2,34 @@ class GroupsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    @groups = Group.all.map do |g|
+      {
+        group: g
+      }
+    end
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def create
+    name = params[:name]
+    
+    group = Group.create(name: name)
+
+    redirect_to group_url(group), flash: {success: "You have successfully created the group #{name}. "}
   end
 
   def destroy
+    group = Group.find(params[:id])
+
+    group.destroy
+    @group = group
+
+    respond_to do |format|
+      format.js
+    end
+
   end
 end
